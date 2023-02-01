0b91b3200dfe2b70be66a205143f327777523473
nshem <nogas@stackpulse.io>
2023-02-01 16:59:33 +0200

    add proto to matcher (#10)
    Co-authored-by: nshem <nogas@torq.io>

--- pkg/mocker/matchers.go
+++ pkg/mocker/matchers.go
@@ -1,6 +1,11 @@
 package mocker
 
-import "reflect"
+import (
+	"reflect"
+
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/reflect/protoreflect"
+)
 
 func Any() Matcher             { return &anyMatcher{} }
 func Eq(x interface{}) Matcher { return &eqMatcher{x} }
@@ -22,6 +27,11 @@ func (e eqMatcher) Matches(x interface{}) bool {
 		return reflect.DeepEqual(e.x, x)
 	}
 
+	// If inputs are proto messages, use proto.Equal
+	if match, isProto := protoMatches(e.x, x); isProto {
+		return match
+	}
+
 	// Check if types assignable and convert them to common type
 	x1Val := reflect.ValueOf(e.x)
 	x2Val := reflect.ValueOf(x)
@@ -32,4 +42,18 @@ func (e eqMatcher) Matches(x interface{}) bool {
 	}
 
 	return false
+}
+
+func protoMatches(a, b interface{}) (isMatching, isProto bool) {
+	aProto, ok := a.(protoreflect.ProtoMessage)
+	if !ok {
+		return isMatching, false
+	}
+
+	bProto, ok := b.(protoreflect.ProtoMessage)
+	if !ok {
+		return isMatching, false
+	}
+
+	return proto.Equal(aProto, bProto), true
 }

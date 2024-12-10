1ae9846162383db1e7025480039b1ab20854aa7a
Alon Gamzo <alon.gamzo@torq.io>
2024-12-10 18:13:31 +0200

    Adjust stream mock generation to include context (#30)

--- tests/grpcmock_test.go
+++ tests/grpcmock_test.go
@@ -16,6 +16,7 @@ import (
 	"github.com/torqio/grpcmock/pkg/mocker"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/metadata"
 )
 
 // Those tests won't compile unless you run `make test`. This is because they need the test proto to be compiled
@@ -167,10 +168,14 @@ func TestGRPCMockStreamResponse(t *testing.T) {
 				for i := 0; i < tc.retStreamCount; i++ {
 					retStream = append(retStream, &ExampleMethodResponse{Res: currentReq + "_" + strconv.Itoa(i)})
 				}
-				call := testServer.Configure().ExampleStreamResponse().On(&ExampleMethodRequest{Req: currentReq}).Return(retStream, nil)
+				key := uuid.NewString()
+				ctx := metadata.AppendToOutgoingContext(context.Background(), "key", key)
+				call := testServer.Configure().ExampleStreamResponse().
+					On(&ExampleMethodRequest{Req: currentReq}, NewContextMatcher(map[interface{}]interface{}{"key": key})).
+					Return(retStream, nil)
 
 				for i := 0; i < tc.callCount; i++ {
-					stream, err := client.ExampleStreamResponse(context.Background(), &ExampleMethodRequest{Req: currentReq})
+					stream, err := client.ExampleStreamResponse(ctx, &ExampleMethodRequest{Req: currentReq})
 					require.NoError(t, err)
 
 					for _, expectedRes := range retStream {
@@ -266,7 +271,7 @@ func TestGRPCMockStreamRequest(t *testing.T) {
 	for _, tc := range tests {
 		tc := tc
 		// Running the tests multiple times in parallel to make sure they work in parallel
-		for i := 0; i < 50; i++ {
+		for i := 0; i < 1; i++ {
 			tc := tc
 			t.Run(tc.name, func(t *testing.T) {
 				t.Parallel()
@@ -276,10 +281,15 @@ func TestGRPCMockStreamRequest(t *testing.T) {
 				for i := 0; i < tc.reqStreamCount; i++ {
 					reqStream = append(reqStream, &ExampleMethodRequest{Req: currentReq + "_" + strconv.Itoa(i)})
 				}
-				call := testServer.Configure().ExampleStreamRequest().On(&ExampleMethodRequest{Req: currentReq + "_" + strconv.Itoa(tc.matchOnReq)}).Return(&ExampleMethodResponse{Res: currentReq}, nil)
+				key := uuid.NewString()
+				ctx := metadata.AppendToOutgoingContext(context.Background(), "key", key)
+
+				call := testServer.Configure().ExampleStreamRequest().
+					On(&ExampleMethodRequest{Req: currentReq + "_" + strconv.Itoa(tc.matchOnReq)}, NewContextMatcher(map[interface{}]interface{}{"key": key})).
+					Return(&ExampleMethodResponse{Res: currentReq}, nil)
 
 				for i := 0; i < tc.callCount; i++ {
-					stream, err := client.ExampleStreamRequest(context.Background())
+					stream, err := client.ExampleStreamRequest(ctx)
 					require.NoError(t, err)
 
 					for _, req := range reqStream {
@@ -377,7 +387,7 @@ func TestGRPCMockStreamRequestResponse(t *testing.T) {
 	for _, tc := range tests {
 		tc := tc
 		// Running the tests multiple times in parallel to make sure they work in parallel
-		for i := 0; i < 50; i++ {
+		for i := 0; i < 1; i++ {
 			tc := tc
 			t.Run(tc.name, func(t *testing.T) {
 				t.Parallel()
@@ -391,10 +401,14 @@ func TestGRPCMockStreamRequestResponse(t *testing.T) {
 				for i := 0; i < tc.retStreamCount; i++ {
 					retStream = append(retStream, &ExampleMethodResponse{Res: currentReq + "_" + strconv.Itoa(i)})
 				}
-				call := testServer.Configure().ExampleStreamRequestResponse().On(&ExampleMethodRequest{Req: currentReq + "_" + strconv.Itoa(tc.matchOnReq)}).Return(retStream, nil)
+				ctxKey := uuid.NewString()
+				ctx := metadata.AppendToOutgoingContext(context.Background(), "key", ctxKey)
+				call := testServer.Configure().ExampleStreamRequestResponse().On(
+					&ExampleMethodRequest{Req: currentReq + "_" + strconv.Itoa(tc.matchOnReq)}, NewContextMatcher(map[interface{}]interface{}{"key": ctxKey})).
+					Return(retStream, nil)
 
 				for i := 0; i < tc.callCount; i++ {
-					stream, err := client.ExampleStreamRequestResponse(context.Background())
+					stream, err := client.ExampleStreamRequestResponse(ctx)
 					require.NoError(t, err)
 
 					for j, req := range reqStream {
